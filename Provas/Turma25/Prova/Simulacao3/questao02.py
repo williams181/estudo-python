2e8b9998d2f2467e142d6ad13bc97d9d1e6c99ac
williams181 <wras@softex.cin.ufpe.br>
2024-06-20 11:45:57 -0300

    pratica

---
+++ Provas/Turma25/Prova/Simulacao3/questao02.py
@@ -0,0 +1,30 @@
+maximo = 150
+pos2d = [0] * maximo
+qtd = qtdNeg = 0
+maiorMult7 = -999999999
+
+num = int(input("Digite um numero para iniciar: "))
+
+while num > 0:
+	num = int(input("Digite um numero negativo: "))
+	
+
+while (num < 0) and (qtd < maximo):
+	qtd += 1
+	if (num > -100) and (num < -9) and (num % 5 == 0):
+		pos2d[qtdNeg] = num
+		qtdNeg += 1
+	if (num % 7 != 0) and (num > maiorMult7):
+		maiorMult7 = num
+	num = int(input("Digite um numero negativo (0 p/parar): "))
+
+if qtdNeg == 0:
+	print("Não foram digitados numeros negativos com 2 digitos significativos.")
+else:
+	for i in range(qtdNeg):
+		print(pos2d[i])
+
+if maiorMult7 == -999999999:
+	print("Não foram digitados numeros não multiplos de 7.")
+else:
+	print("o maior numero lido que não e multiplo de 7 é: ",maiorMult7)
---
+++ Provas/Turma25/Prova/Simulacao3/questao03.py
@@ -0,0 +1,32 @@
+def MelhoresClientes(arquivo, pontuacao):
+	try:
+		grav = 0
+		medPont = 0
+		arqEnt = open("C://william/" + arquivo + "txt", "r")
+		arqSai = open("C://william/" + arquivo + "Melhores.txt", "w")
+		for linha in arquivo:
+			matricula = linha[0:5]
+			sexo = linha[5:6]
+			pontos = linha[6:13]
+			postInt = int(pontos)
+			nome = linha[13:49]
+			if (postInt > pontuacao):
+				arqSai.write("%s, %d," % (matricula, postInt))
+				grav += 1	
+			arqEnt.close()
+			arqSai.clone()
+		print("Arquivo gravado com sucesso. ",arquivo)
+	except IOError:
+		print("Erro ao gravar o arquivo. Tente novamente")
+
+n = int(input("Digite a quantidade de arquivos que deseja processar: "))
+
+while n < 1:
+	n = int(input("Digite a quantidade de arquivos que deseja processar: "))
+
+for i in range(n):
+	nome = input("Digite o nome do cliente: ")
+	pont = input("Digite a pontuacao deste cliente: ")
+	
+	MelhoresClientes(nome, pont)
+print("Fim do programa.")
